918ce2afbff96c55d1553a4b5e67caaef71d1647
Muhammad Kazim Raza <rkazim248@gmail.com>
2026-06-17 20:22:50 +0500

    Add files via upload

---
+++ Assisment 2/assisment1.py
@@ -0,0 +1,55 @@
+#Task 1
+name = "Kazim"
+age = 15
+city = "Karachi"
+isStudent = True
+
+print("---Taks 1---");
+print(name);
+print(age);
+print(city);
+print(isStudent);
+
+#Task 2
+print("---Taks 2---");
+print(type(name));
+print(type(age));
+print(type(isStudent));
+
+#Task 3
+marks = "95"
+converted_marks = int(marks)
+print("---Taks 3---");
+print(converted_marks);
+print(type(converted_marks));
+
+#Task 4
+phoneNumber = 1234567890
+converted_phoneNumber = str(phoneNumber)
+print("---Taks 4---");
+print(converted_phoneNumber);
+print(type(converted_phoneNumber));
+
+#Task 5
+userName = "Ahmed"
+converted_userName = bool(userName)
+print("---Taks 5---");
+print(converted_userName);
+print(type(converted_userName));
+
+#Task 6
+p_name = "kazim"
+p_age = 15
+p_favorite_color = "blue"
+p_student = True
+print("---Taks 6---");
+print("Name:", p_name);
+print("Age:", p_age);
+print("Favorite Color:", p_favorite_color);
+print("Is Student:", p_student);    
+
+#Bonce Challenge
+num = "100"
+converted_num = int(num)
+print("---Bonce Challenge---");
+print(converted_num + 50);
