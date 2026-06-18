03e2e5089202e585b4e6fac227a04b6f4d8db8be
Muhammad Kazim Raza <rkazim248@gmail.com>
2026-06-18 10:32:13 +0500

    Add files via upload

---
+++ Assisment 3/assignment.py
@@ -0,0 +1,45 @@
+#Task 1
+print("Task 1")
+print("My name is Kazim");
+print("I am learning Python");
+print("Python is fun!");
+
+#Task 2
+name = "Kazim"
+age = 15
+city = "Karachi"
+
+print("Task 2")
+print(name);
+print(age);
+print(city);
+
+#Task 3
+num1 = 15
+num2 = 10
+
+print("Task 3")
+print(num1 + num2);
+
+#Task 4
+a = 8 
+b = 5
+
+print("Task 4")
+print(a * b);
+
+#Task 5
+print("Task 5");
+print(20 + 10);
+print(20 - 10);
+print(20 * 10);
+print(20 / 10);
+print(20 % 3);
+print(20 // 3);
+print(2 ** 4);
+
+#Bonus Task
+favorite_number = 7
+
+print("Bonus Task")
+print(favorite_number);
